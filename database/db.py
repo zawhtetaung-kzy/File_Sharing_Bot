65a6c154ce5d123c07f3a7ca9757dd7408a6bce6
Zaw Htet Aung <zawhtetaung.coms@gmail.com>
2025-08-30 01:22:54 +0630

    Update db.py

--- database/db.py
+++ database/db.py
@@ -7,18 +7,14 @@ class Database:
         self.db = self.client["FileSharingBot"]
         self.files_col = self.db["files"]
 
-    async def add_file(self, movie_name, file_size, message_id):
+    async def add_file(self, file_name, file_size, file_id):
         await self.files_col.update_one(
-            {"movie_name": movie_name},
-            {"$set": {"file_size": file_size, "message_id": message_id}},
+            {"file_name": file_name},
+            {"$set": {"file_size": file_size, "file_id": message_id}},
             upsert=True,
         )
 
     async def get_files(self, query):
-        # find() ကနေ ရလာတဲ့ cursor object ကို list အနေနဲ့ ပြောင်းပေးခြင်း
-        return await self.files_col.find({"movie_name": {"$regex": query, "$options": "i"}}).to_list(length=10)
-    
-    async def get_file_by_message_id(self, message_id):
-        return await self.files_col.find_one({"message_id": message_id})
+        return self.files_col.find({"file_name": {"$regex": query, "$options": "i"}}).to_list(length=10)
 
 db = Database()
